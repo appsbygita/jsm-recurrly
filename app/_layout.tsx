686c02d94ab42886fa2f63a7e665011ffa038350
Gita Ventyana <gita.ventyana@gmail.com>
2026-04-13 22:31:26 +0700

    add tab layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,5 +2,10 @@ import "@/global.css";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack>
+      {/* Hide the header for the tabs group if you want */}
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
